8985cd0e3d95d080945c8cd5556b102d5200eb4d
onionmccabbage <onionmccabbage@live.com>
2023-10-23 16:28:56 +0100

    end of day 1

---
+++ loading_api/use_req.py
@@ -0,0 +1,14 @@
+import requests
+
+# the 'requests' library may need to be added to python
+# pip install requests
+# pip3 install requsts
+# python -m pip install requests
+
+# in earlier python the urllib3 was widely used. The requests library is much more elegant
+
+def getData():
+    '''fetch some data from an external API'''
+
+if __name__ == '__main__':
+    getData()
