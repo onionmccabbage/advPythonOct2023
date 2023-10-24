803ee5b5cf8fac4de9fc1e54a3d213977f6decdc
onionmccabbage <onionmccabbage@live.com>
2023-10-24 16:04:40 +0100

    example custom generator

--- custom_gen.py
+++ custom_gen.py
@@ -8,6 +8,22 @@ def demo():
     # the benefit of generators is that at no time do the values all exist in memory
     # the values are generated on demand from a range or a generator object
 
+def my_gen():
+    '''generate never ending exponential values and yield the results'''
+    n=1
+    while True: # careful - this is an endless loop
+        exp = n*n
+        n+=1
+        yield exp # the yield statement makes this function a generator
 
 if __name__ == '__main__':
-    demo()
+    # demo()
+    g = my_gen()
+    print(type(g)) # we have a generator
+    print( g.__next__() ) # 1
+    print( g.__next__() ) # 4
+    print( g.__next__() ) # 9
+    # we can choose to iterate and use our generator
+    for i in range(1, 25):
+        print( g.__next__() )
+        
