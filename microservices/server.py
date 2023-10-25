be6c58aac2d3c83ba82d8f7eb930c290c736fa03
onionmccabbage <onionmccabbage@live.com>
2023-10-25 11:45:47 +0100

    slightly improved microservice (with logging)

--- microservices/server.py
+++ microservices/server.py
@@ -1,9 +1,23 @@
 # 'socket' provides all we need for socket programming (ie server/client)
 import socket
+import sys
 
+def writeToFile(data, fileName):
+    '''we can log the server activity to a file'''
+    # we really should use try-except
+    fout = open(fileName, 'a')
+    fout.write(f'{data}\n') # make sure every commit has a new line at the end
+    fout.close()
+
+# within this server we will send output to our log file
+# this could be done using redirection
 def server():
     '''this microservice will listen for requests and respond
     It will be available over an http port (IP Internet Protocol)'''
+    # see if any sys.argv are provided (for the log file name)
+    fileName = 'serverlog.txt'
+    if len(sys.argv) > 1:
+        fileName = sys.argv[1]
     # we need some port settings
     port_t = ('localhost', 9874) # or a fixed IP address
     # now we create a server. Here are sensible defaults
@@ -11,19 +25,19 @@ def server():
     server.bind(port_t)
     # we can begin listening for requests
     server.listen() # we can set a listening backlog...
-    print(f'Server is listening on port {port_t[1]} address {port_t[0]}')
+    writeToFile(f'Server is listening on port {port_t[1]} address {port_t[0]}', fileName)
     # we need a 'run loop'
     while True: # endless loop
         # unpack any request received
         (client, addr) = server.accept() 
         # print(f'Request received from {client}, {addr}')
-        print(f'Request received from {addr}')
+        writeToFile(f'Request received from {addr}', fileName)
         # read a portion of the client stream
         buf = client.recv(1024) # just the first 1024 bytes
-        print(f'server received {buf}')
+        writeToFile(f'server received {buf}', fileName)
         # we can provide a mechanism to stop the server
         if buf==b'quit':
-            print('Server is closing')
+            writeToFile('Server is closing', fileName)
             server.close()
             break # stop the run-loop
 
