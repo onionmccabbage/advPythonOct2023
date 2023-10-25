d6e3d123ded408945757273391b8be95342ed6d8
onionmccabbage <onionmccabbage@live.com>
2023-10-25 12:15:45 +0100

    doctest example

--- testing/using_doctest.py
+++ testing/using_doctest.py
@@ -12,7 +12,22 @@ def nthPower(d=2, p=4):
     '''
     return d**p
 
+def cubeIt(a, b):
+    '''
+    return all the cubes of numbers from a to b
+    >>> cubeIt(3, 8)
+    [27, 64, 125, 216, 343]
+    >>> cubeIt(1, 101) # doctest:+ELLIPSIS
+    [1, 8, ..., 1000000]
+    '''
+    answers = []
+    for i in range(a, b):
+        answers.append( i**3 )
+    return answers
+
 if __name__ == '__main__':
     # result = nthPower(2, 4)
     # print(result)
     doctest.testmod(verbose=True)
+    # c = cubeIt(1, 9)
+    # print(c)
