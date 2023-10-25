93353a99bdc74c450536ee9e2d4cb97d4426a524
onionmccabbage <onionmccabbage@live.com>
2023-10-25 10:59:28 +0100

    client

--- microservices/client.py
+++ microservices/client.py
@@ -1,5 +1,7 @@
 # we can build a socket client
 import socket
+# we can read system argument variables
+import sys
 
 def client():
     '''this is a basic socket client to interact with our socket server'''
@@ -7,7 +9,11 @@ def client():
     port_t = ('localhost', 9874)
     client.connect(port_t)
     # we can send a message to the server
-    message = 'quit'
+    # check if there were aditional system argument variables
+    if len(sys.argv) > 1: # there will always be at least one sys.argv (the module name)
+        message = ' '.join(sys.argv[1:]) # ignore member zero
+    else:
+        message = 'default' # we can send 'quit' as a system argument variable
     # all http traffic must be url-encoded
     client.send(message.encode())
     # we can handle any response from the server
---
+++ microservices/using_sysarg.py
@@ -0,0 +1,5 @@
+import sys
+
+# we can inject values into any python module when we run it
+# these are called system argument variables
+print(sys.argv) # there will ALWAYS be one sys.argv - the current running module path
