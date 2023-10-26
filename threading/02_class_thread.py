0bfad6df7ed19b222bace0de2109884ff1f1cd04
onionmccabbage <onionmccabbage@live.com>
2023-10-26 10:30:16 +0100

    four class threads

--- threading/02_class_thread.py
+++ threading/02_class_thread.py
@@ -14,14 +14,23 @@ class MyClass(Thread):
         '''When this (Thread) is invoked, thus run method will execute
         This makes the instance run on its own thread'''
         for i in range(0,10): # on average, half a second overall
-            print(f'{self.n} us sleeping for {self.t}')
-            time.sleep(self.t)
+            print(f'{self.n} is sleeping') # careful: printing takes significant time
+            time.sleep(random.random()*0.1) # each iteration will be different
 
 if __name__ == '__main__':
     # random.random()*0.1 will sleep between 0 and 0.1 second
-    t1 = MyClass(1, random.random()*0.1) # this is effectively an instance of a Thread
+    t1 = MyClass(1, 1) # this is effectively an instance of a Thread
+    t2 = MyClass(2, 2) # this is effectively an instance of a Thread
+    t3 = MyClass(3, 2) # this is effectively an instance of a Thread
+    t4 = MyClass(4, 2) # this is effectively an instance of a Thread
     start = time.time()
     t1.start()
-
+    t2.start()
+    t3.start()
+    t4.start()
+    t1.join() # this blocks the main thread until the sub thread joins back
+    t2.join() # this blocks the main thread until the sub thread joins back
+    t3.join() # this blocks the main thread until the sub thread joins back
+    t4.join() # this blocks the main thread until the sub thread joins back
     end = time.time()
     print(f'Overall time was {end-start}')
